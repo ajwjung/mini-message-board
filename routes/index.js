8b68a9ff91f017c31f23239b043fa31d88a48bab
Amy Jung <ajwjung@outlook.com>
2024-06-14 17:42:43 -0700

    feat: User can submit a new message

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// Store our message data
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+/* GET home page */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Messageboard', messages });
+});
+
+/* GET new form page */
+router.get("/new", function(req, res, next) {
+  res.render("form", { title: "New Form" })
+});
+
+/* POST data from new form */
+router.post("/new", function(req, res, next) {
+  const messageUser = req.body.messageUser;
+  const messageText = req.body.messageText;
+  
+  // Add the newest message submission to our data array
+  messages.push({ text: messageText, user: messageUser, added: new Date() });
+
+  // Send users back to index page after submitting a message
+  res.redirect("/");
 });
 
 module.exports = router;
